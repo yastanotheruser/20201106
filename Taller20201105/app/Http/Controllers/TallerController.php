37c565e2d2a5f814a9deb33dd5c97c63e6ab2471
Jhan S. Álvarez <alvarezpcuser@gmail.com>
2020-11-05 16:59:13 -0500

    adición TallerController

---
+++ Taller20201105/app/Http/Controllers/TallerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TallerController extends Controller
+{
+    public function index() {
+        echo "Hola Mundo";
+        return "Hola Mundo";
+    }
+}
